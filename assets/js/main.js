9966b0ea820ae755e023f3f8085efbc741911e0a
162tmac <tom.m.mcevoy@gmail.com>
2020-11-01 11:56:14 +0000

    Added JS functionality to booking form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
-document.querySelector('#children').addEventListener('change', ()=>{
-    if (document.querySelector('#children').value === 1) {
-        document.querySelector('#child2').style.display = "none";
-        document.querySelector('#child3').style.display = "none";
-    } else if (document.querySelector('#children').value === 2) {
-        document.querySelector('#child2').style.display = "block";
-        document.querySelector('#child3').style.display = "none";
-    } else if (document.querySelector('#children').value === 3) {
-        document.querySelector('#child2').style.display = "block";
-        document.querySelector('#child3').style.display = "block";
+document.querySelector('#children').addEventListener("change", ()=>{
+    let noOfChildren = document.querySelector('#children').value;
+    let child2 = document.querySelector('#child2');
+    let child3 = document.querySelector('#child3');
+
+    if (noOfChildren === "1") {
+        child2.style.display = "none";
+        child2.required = false;
+        child3.style.display = "none";
+        child3.required = false;
+    } else if (noOfChildren === "2") {
+        child2.style.display = "block";
+        child2.required = true;
+        child3.style.display = "none";
+        child3.required = false;
+    } else if (noOfChildren === "3") {
+        child2.style.display = "block";
+        child2.required = true;
+        child3.style.display = "block";
+        child3.required = true;
     }
 })
